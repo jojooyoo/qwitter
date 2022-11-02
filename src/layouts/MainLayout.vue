23f5e33e7c592f6f1bae92e17983cc4178c85ab7
jojooyoo <115844041+jojooyoo@users.noreply.github.com>
2022-11-02 15:18:41 +0200

    test

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,102 @@
+<template>
+  <q-layout view="lHr lpR fFf">
+
+    <q-header bordered class="bg-white text-black">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title class="text-weight-bold">
+          Qwitter
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" :width="283">
+      <q-icon 
+      class="q-pa-md"
+       name="today"
+       size="lg"  
+       color="primary"    
+      />
+
+      <q-list>
+      <q-item 
+        to="/"
+        clickable 
+        v-ripple
+      >
+        <q-item-section avatar>
+          <q-icon name="home" size="md"/>
+        </q-item-section>
+
+        <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+      </q-item>
+      
+            <q-item 
+        to="/about"
+        clickable 
+        v-ripple
+      >
+        <q-item-section avatar>
+          <q-icon name="info" size="md"/>
+        </q-item-section>
+
+        <q-item-section class="text-h6 text-weight-bold">About</q-item-section>
+      </q-item>
+
+      </q-list>
+
+    </q-drawer>
+
+   <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+      <q-input placeholder="search qwitter" class="q-ma-md" outlined rounded dense>
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input> 
+
+     <q-list padding>
+      <q-item>
+       <q-item-section>
+          <q-item-label overline>OVERLINE</q-item-label>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+       </q-item-section>
+      
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+     </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+    const rightDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+
+      rightDrawerOpen,
+      toggleRightDrawer () {
+        rightDrawerOpen.value = !rightDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
